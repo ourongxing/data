95764471a7c3a902e28c8f2072c7ddf861a17af4
ourongxing <orongxing@gmail.com>
2021-01-28 23:46:11 +0800

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-// version v0.1
-// create by ourongxing
-// detail url: https://github.com/ourongxing/CQUPT-Health-ClockIn
-
-
-// 获取当前时间戳
-const getTimeStamp = () => Math.floor(Date.now() / 1000);
-console.log(getTimeStamp())
